7370581354edf95736ab5e849922b4af3ef21503
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2024-03-11 22:34:00 +0100

    build with pdm

--- boldi/dev/cli.py
+++ boldi/dev/cli.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from typing import Callable
+from typing import Callable, List
 
 import boldi.cli
 
@@ -12,8 +12,9 @@ class DevCliAction(boldi.cli.CliAction):
         subparser.set_defaults(dev_action=self.do_help)
         subparser.usage = "run a dev command"
         dev_subparsers = subparser.add_subparsers()
-        dsp_package = dev_subparsers.add_parser("package")
-        dsp_package.set_defaults(dev_action=self.do_package)
+        subparser_package = dev_subparsers.add_parser("package")
+        subparser_package.set_defaults(dev_action=self.do_package)
+        subparser_package.add_argument("--only-include", "-i", nargs="+", default=[], help="only include these packages")
 
     def do_action(self, dev_action: Callable[..., None], **kwargs):
         dev_action(**kwargs)
@@ -21,5 +22,5 @@ class DevCliAction(boldi.cli.CliAction):
     def do_help(self):
         self.subparser.print_help(self.ctx.stderr)
 
-    def do_package(self):
-        package(self.ctx)
+    def do_package(self, only_include: List[str]):
+        package(self.ctx, only_include)
--- boldi/dev/package.py
+++ boldi/dev/package.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 from boldi.cli import CliCtx
 
 
-def package(ctx: CliCtx):
+def package(ctx: CliCtx, only_include: List[str]):
+    include_set = set(only_include)
     ctx.console.print("[green]package[/]")
     root: Optional[Path] = None
     for parent_dir in [ctx.cwd] + list(ctx.cwd.parents):
@@ -16,5 +17,8 @@ def package(ctx: CliCtx):
         return
 
     for pkg in sorted((root / "pkg").iterdir()):
+        if include_set and pkg.name not in include_set:
+            continue
+
         if pkg.is_dir() and (pkg / "pyproject.toml").is_file():
-            ctx.run_py("-m build --no-isolation --sdist --wheel", ["--outdir", root / "dist"], [pkg])
+            ctx.run_py("-m build --no-isolation --sdist --wheel", ["--outdir", root / "dist"], cwd=pkg)
