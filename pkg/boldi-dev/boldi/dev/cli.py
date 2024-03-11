500ee58f1bcfec2cb276a85108da7e9a209fcb59
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2024-03-11 23:43:34 +0100

    boldi/cli.py: format + absolute import

--- pkg/boldi-dev/boldi/dev/cli.py
+++ pkg/boldi-dev/boldi/dev/cli.py
@@ -2,8 +2,7 @@ from argparse import ArgumentParser
 from typing import Callable, List
 
 import boldi.cli
-
-from .package import package
+import boldi.dev.package
 
 
 class DevCliAction(boldi.cli.CliAction):
@@ -14,7 +13,9 @@ class DevCliAction(boldi.cli.CliAction):
         dev_subparsers = subparser.add_subparsers()
         subparser_package = dev_subparsers.add_parser("package")
         subparser_package.set_defaults(dev_action=self.do_package)
-        subparser_package.add_argument("--only-include", "-i", nargs="+", default=[], help="only include these packages")
+        subparser_package.add_argument(
+            "--only-include", "-i", nargs="+", default=[], help="only include these packages"
+        )
 
     def do_action(self, dev_action: Callable[..., None], **kwargs):
         dev_action(**kwargs)
@@ -23,4 +24,4 @@ class DevCliAction(boldi.cli.CliAction):
         self.subparser.print_help(self.ctx.stderr)
 
     def do_package(self, only_include: List[str]):
-        package(self.ctx, only_include)
+        boldi.dev.package.package(self.ctx, only_include)
