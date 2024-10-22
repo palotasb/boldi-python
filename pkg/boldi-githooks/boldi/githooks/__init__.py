1180a3da3a8764bee5008e906dbf9375a2ca0e36
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2024-10-22 22:25:23 +0200

    githooks: use single githooks.py script

--- pkg/boldi-githooks/boldi/githooks/__init__.py
+++
@@ -1,6 +0,0 @@
-from boldi.githooks._pygithooks import cli_githooks, main
-
-__all__ = [
-    "cli_githooks",
-    "main",
-]
--- pkg/boldi-githooks/boldi/githooks/__main__.py
+++
@@ -1,4 +0,0 @@
-from boldi.githooks import main
-
-if __name__ == "__main__":
-    main()
