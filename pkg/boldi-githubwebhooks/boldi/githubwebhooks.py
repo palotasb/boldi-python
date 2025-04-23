062d2470838d50d34552e18009ee8d7005bc8e48
Boldizsár Palotás <boldizsar.palotas@gmail.com>
2025-04-23 05:50:11 +0200

    add boldi-githubwebhooks

---
+++ pkg/boldi-githubwebhooks/boldi/githubwebhooks.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from fastapi import FastAPI, Header
+from loguru import logger
+from pydantic import BaseModel
+from starlette.responses import PlainTextResponse
+
+logger.disable(__name__)
+
+
+class GitHubWebhook(BaseModel):
+    gh_hook_id: str = Header(alias="X-GitHub-Hook-ID")
+    action: str
+    issue: dict
+    repository: dict
+    sender: dict
+
+
+class GitHubWebhooksApp(FastAPI):
+    def __init__(self, root_path: str = "") -> None:
+        super().__init__(root_path=root_path)
+
+        @self.get("/")
+        async def post_github_webhook():
+            return PlainTextResponse(content="OK")
+
+    async def __call__(self, scope, receive, send):
+        logger.info(f"{scope=} {receive=} {send=}")
+        return await super().__call__(scope, receive, send)
