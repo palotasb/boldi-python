743d9b80b351eab1b9d7fa36e7cb20002cfeabfd
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2025-04-29 07:28:47 +0200

    switch to fastapi, remove server dependencies

--- pkg/boldi-githubwebhooks/boldi/githubwebhooks.py
+++ pkg/boldi-githubwebhooks/boldi/githubwebhooks.py
@@ -1,18 +1,29 @@
+from __future__ import annotations
+
+from fastapi import FastAPI, Header
 from loguru import logger
-from starlette.applications import Starlette
-from starlette.requests import Request
-from starlette.responses import Response
+from pydantic import BaseModel
+from starlette.responses import PlainTextResponse
 
-# logger.disable(__name__)
+logger.disable(__name__)
 
 
-class GitHubWebhooksApp(Starlette):
-    def __init__(self) -> None:
-        super().__init__()
-        self.add_route("/", self.post_github_webhook, methods=["GET", "POST"])
+class GitHubWebhook(BaseModel):
+    gh_hook_id: str = Header(alias="X-GitHub-Hook-ID")
+    action: str
+    issue: dict
+    repository: dict
+    sender: dict
 
-    async def post_github_webhook(self, request: Request) -> Response:
-        logger.info("{} {} {}", request.method, request.url, vars(request))
-        content = await request.body()
-        logger.info("content: {}", content)
-        return Response(content=content, status_code=200, media_type=request.headers.get("Content-Type"))
+
+class GitHubWebhooksApp(FastAPI):
+    def __init__(self, root_path: str = "") -> None:
+        super().__init__(root_path=root_path)
+
+        @self.get("/")
+        async def post_github_webhook():
+            return PlainTextResponse(content="OK")
+
+    async def __call__(self, scope, receive, send):
+        logger.info(f"{scope=} {receive=} {send=}")
+        return await super().__call__(scope, receive, send)
