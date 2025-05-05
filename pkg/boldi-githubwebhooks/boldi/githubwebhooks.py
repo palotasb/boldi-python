1ab79a15a48be4ddbdf7efa9cb2a0171015f03d2
Boldizsár Palotás <boldizsar.palotas@gmail.com>
2025-05-05 16:01:36 +0200

    add push hook class definition

--- pkg/boldi-githubwebhooks/boldi/githubwebhooks.py
+++ pkg/boldi-githubwebhooks/boldi/githubwebhooks.py
@@ -30,22 +30,72 @@ class GitHubWebhookHeaders(BaseModel):
 
 
 class PushGitHubWebhook(BaseModel):
+    # The SHA of the most recent commit on `ref` after the push.
     after: str
+    # ?
     base_ref: str | None = None
+    # The SHA of the most recent commit on `ref` before the push.
     before: str
-    commits: list[dict]
+    # An array of commit objects describing the pushed commits. (Pushed commits are all commits that are included in the compare between the before commit and the after commit.) The array includes a maximum of 2048 commits. If necessary, you can use the Commits API to fetch additional commits.
+    commits: list[Commit]
+    # URL that shows the changes in this ref update, from the before commit to the after commit. For a newly created ref that is directly based on the default branch, this is the comparison between the head of the default branch and the after commit. Otherwise, this shows all commits until the after commit.
     compare: str
+    # Whether this push created the `ref`.
     created: bool
+    # Whether this push deleted the `ref`.
     deleted: bool
+    # An enterprise on GitHub. Webhook payloads contain the enterprise property when the webhook is configured on an enterprise account or an organization that's part of an enterprise account. For more information, see "About enterprise accounts."
     enterprise: dict | None = None
+    # Whether this push was a force push of the `ref`.
     forced: bool
-    head_commit: dict | None = None
+    # TODO
+    head_commit: Commit | None  # may be None, but is required
+    # The GitHub App installation. Webhook payloads contain the installation property when the event is configured for and sent to a GitHub App. For more information, see "Using webhooks with GitHub Apps."
     installation: dict | None = None
+    # A GitHub organization. Webhook payloads contain the organization property when the webhook is configured for an organization, or when the event occurs from activity in a repository owned by an organization.
     organization: dict | None = None
-    pusher: dict
+    # Metaproperties for Git author/committer information.
+    pusher: User
+    # The full git ref that was pushed. Example: `refs/heads/main` or `refs/tags/v3.14.1`.
     ref: str
+    # TODO A git repository ???
     repository: dict
+    # A GitHub user.
     sender: dict | None = None
+
+
+class Commit(BaseModel):
+    # An array of files added in the commit. A maximum of 3000 changed files will be reported per commit.
+    added: list[str]
+    # Metaproperties for Git author information.
+    author: User
+    # Metaproperties for Git committer information.
+    committer: User
+    # Whether this commit is distinct from any that have been pushed before.
+    distinct: bool
+    # ID
+    id: str
+    # The commit message.
+    message: str
+    # An array of files modified by the commit. A maximum of 3000 changed files will be reported per commit.
+    modified: list[str]
+    # An array of files removed in the commit. A maximum of 3000 changed files will be reported per commit.
+    removed: str
+    # The ISO 8601 timestamp of the commit.
+    timestamp: str  # TODO type as datetime
+    # Tree ID
+    tree_id: str
+    # URL that points to the commit API resource.
+    url: str
+
+
+class User(BaseModel):
+    """Metaproperties for Git author/committer information."""
+
+    date: str | None = None
+    email: str | None  # may be None, but is required
+    name: str
+    username: str | None = None
 
 
 class GitHubWebhooksApp(FastAPI):
@@ -59,12 +109,20 @@ class GitHubWebhooksApp(FastAPI):
         @self.post("/")
         async def post_github_webhook(
             headers: Annotated[GitHubWebhookHeaders, Header()],
-            hook: PushGitHubWebhook | dict | None = None,
+            hook: PushGitHubWebhook | dict,
         ) -> dict:
+            match hook:
+                case push if isinstance(push, PushGitHubWebhook):
+                    return await handle_post_hook(headers, push)
+                case hook if isinstance(hook, dict):
+                    return {"headers": headers, "hook": hook}
+                case _:
+                    assert False, "unreachable, otherwise internal error"
+
+        async def handle_post_hook(headers: GitHubWebhookHeaders, push: PushGitHubWebhook) -> dict:
             return {
                 "headers": headers,
-                "hook_type": type(hook).__name__,
-                "hook": hook,
+                "push": push,
             }
 
     async def __call__(self, scope, receive, send):
